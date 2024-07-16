699a2983839b4514743ccd0a0232efff4538b87f
Nilay1004 <nilaykujur@gmail.com>
2024-07-16 12:47:57 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -214,6 +214,14 @@ after_initialize do
       end
     end
   end
+
+  class ::User
+    def emails
+      self.user_emails.order("user_emails.primary DESC NULLS LAST").pluck(:email).map do |encrypted_email|
+        PIIEncryption.decrypt_email(encrypted_email)
+      end
+    end
+  end
 end
 
 
