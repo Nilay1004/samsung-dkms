6997ca3e04ebb52afe38d18616fcbb0ebf58edcd
Nilay1004 <nilaykujur@gmail.com>
2024-07-17 05:08:41 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -199,22 +199,6 @@ after_initialize do
     end
   end
 
-  if defined?(::EmailToken)
-    class ::EmailToken
-      Rails.logger.info "----------Overrided EmailToken class----------"
-      alias_method :original_email=, :email= if method_defined?(:email=)
-
-      def email=(value)
-        encrypted_email = PIIEncryption.encrypt_email(value)
-        write_attribute(:email, encrypted_email)
-      end
-
-      def email
-        decrypted_email = PIIEncryption.decrypt_email(read_attribute(:email))
-      end
-    end
-  end
-
   class ::User
     def emails
       self.user_emails.order("user_emails.primary DESC NULLS LAST").pluck(:email).map do |encrypted_email|
