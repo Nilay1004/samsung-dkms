729c21f72816ec4f774ac5ab55df9905c4d39307
Nilay1004 <nilaykujur@gmail.com>
2024-07-18 12:56:17 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -222,6 +222,39 @@ after_initialize do
       end
     end
   end
+
+  class ::SkippedEmailLog
+    Rails.logger.info "----------Enhanced SkippedEmailLog class----------"
+    
+    before_save :encrypt_to_address
+    after_initialize :decrypt_to_address
+
+    def to_address
+      @decrypted_to_address ||= PIIEncryption.decrypt_email(read_attribute(:to_address))
+    end
+
+    def to_address=(value)
+      @decrypted_to_address = value
+      encrypted_to_address = PIIEncryption.encrypt_email(value)
+      write_attribute(:to_address, encrypted_to_address)
+    end
+
+    private
+
+    def encrypt_to_address
+      if @decrypted_to_address.present?
+        encrypted_to_address = PIIEncryption.encrypt_email(@decrypted_to_address)
+        write_attribute(:to_address, encrypted_to_address)
+      end
+    end
+
+    def decrypt_to_address
+      if read_attribute(:to_address).present?
+        @decrypted_to_address = PIIEncryption.decrypt_email(read_attribute(:to_address))
+      end
+    end
+  end
+
 end
 
 
