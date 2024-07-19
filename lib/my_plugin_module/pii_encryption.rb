bf62dc898c8869fea31988e421c12cde3ffdf9d5
Nilay1004 <nilaykujur@gmail.com>
2024-07-19 10:09:55 +0000

    Update pii_encryption.rb

--- lib/my_plugin_module/pii_encryption.rb
+++ lib/my_plugin_module/pii_encryption.rb
@@ -8,14 +8,14 @@ module PIIEncryption
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
     request.body = { data: email, pii_type: "email" }.to_json
-    Rails.logger.info "PIIEncryption: Sending encryption request for email: #{email}"
+    
     response = http.request(request)
 
     encrypted_email = JSON.parse(response.body)["encrypted_data"]
-    Rails.logger.info "PIIEncryption: Encrypted email: #{encrypted_email}"
+    
     encrypted_email
   rescue StandardError => e
-    Rails.logger.error "Error encrypting email: #{e.message}"
+    
     email
   end
 
@@ -26,14 +26,14 @@ module PIIEncryption
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
     request.body = { data: email, pii_type: "email" }.to_json
-    Rails.logger.info "PIIEncryption: Sending hash request for email: #{email}"
+    
     response = http.request(request)
 
     email_hash = JSON.parse(response.body)["hashed_data"]
-    Rails.logger.info "PIIEncryption: Email hash: #{email_hash}"
+    
     email_hash
   rescue StandardError => e
-    Rails.logger.error "Error hashing email: #{e.message}"
+    
     email
   end
 
@@ -44,14 +44,14 @@ module PIIEncryption
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
     request.body = { data: encrypted_email }.to_json
-    Rails.logger.info "PIIEncryption: Sending decryption request for encrypted email: #{encrypted_email}"
+   
     response = http.request(request)
 
     decrypted_email = JSON.parse(response.body)["decrypted_data"]
-    Rails.logger.info "PIIEncryption: Decrypted email: #{decrypted_email}"
+    
     decrypted_email
   rescue StandardError => e
-    Rails.logger.error "Error decrypting email: #{e.message}"
+    
     encrypted_email
   end
 end
